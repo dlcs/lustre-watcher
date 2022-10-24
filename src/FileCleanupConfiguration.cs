ab77123b45ee33fef69eac441cd8889e65d44a72
Gary Tierney <gary.tierney@fastmail.com>
2022-10-24 10:30:47 +0100

    Add validation to FileCleanupConfiguration

--- src/FileCleanupConfiguration.cs
+++ src/FileCleanupConfiguration.cs
@@ -1,9 +1,45 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LustreCollector;
 
-public class FileCleanupConfiguration
+public class FileCleanupConfiguration : IValidatableObject
 {
     public string MountPoint { get; set; }
 
     public int CleanupThreshold { get; set; } = 10;
     public int CleanupPeriod { get; set; } = 2_000;
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        var valid = true;
+
+        var mountPointExists = File.Exists(MountPoint);
+        valid &= mountPointExists;
+
+        if (!mountPointExists)
+        {
+            yield return new ValidationResult("MountPoint must exist");
+        }
+
+        var cleanupThresholdValid = CleanupThreshold > 1 && CleanupThreshold < 100;
+        valid &= cleanupThresholdValid;
+
+        if (!cleanupThresholdValid)
+        {
+            yield return new ValidationResult("CleanupThreshold must be in the range of (0,100)");
+        }
+
+        var cleanupPeriodValid = CleanupPeriod > 0;
+        valid &= cleanupPeriodValid;
+
+        if (!cleanupPeriodValid)
+        {
+            yield return new ValidationResult("CleanupPeriod must be non-zero");
+        }
+
+        if (valid)
+        {
+            yield return ValidationResult.Success;
+        }
+    }
 }
