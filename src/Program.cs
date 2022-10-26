ea306b1530eee3d621a9194bcfa078453d3eba45
Gary Tierney <gary.tierney@fastmail.com>
2022-10-26 10:19:15 +0100

    Don't use custom configuration instance

--- src/Program.cs
+++ src/Program.cs
@@ -1,15 +1,11 @@
 using LustreCollector;
 using LustreCollector.Filesystem;
 
-var configuration = new ConfigurationBuilder()
-    .AddJsonFile("appsettings.json", true)
-    .AddEnvironmentVariables("LUSTRE_")
-    .Build();
-
 await Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>
+    .ConfigureHostConfiguration(builder => builder.AddEnvironmentVariables("LUSTRE_"))
+    .ConfigureServices((hostContext, services) =>
     {
-        services.Configure<FileCleanupConfiguration>("FileCleanup", configuration);
+        services.Configure<FileCleanupConfiguration>("FileCleanup", hostContext.Configuration);
         services.AddSingleton<IFilesystemChangeWatcher, NativeFilesystemChangeWatcher>();
         services.AddSingleton(x => new SortedSet<FileRecord>(new LustreFileAccessTimeComparer()));
         services.AddHostedService<FileCleanupWorker>();
