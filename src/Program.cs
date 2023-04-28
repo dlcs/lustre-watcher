e2029c0c2a7f6812229af251c90fd87dae7207b4
Donald Gray <donald.gray@digirati.com>
2023-04-28 17:51:48 +0100

    Add serilog

--- src/Program.cs
+++ src/Program.cs
@@ -1,18 +1,40 @@
 using LustreCollector;
 using LustreCollector.Filesystem;
+using Serilog;
 
-await Host.CreateDefaultBuilder(args)
-    .ConfigureHostConfiguration(builder => builder.AddEnvironmentVariables("LUSTRE_"))
-    .ConfigureServices((hostContext, services) =>
-    {
-        services.AddOptions<FileCleanupConfiguration>()
-            .Bind(hostContext.Configuration)
-            .ValidateDataAnnotations();
-        
-        services.AddSingleton<IFilesystemChangeWatcher, NativeFilesystemChangeWatcher>();
-        services.AddSingleton(x => new SortedSet<FileRecord>(new LustreFileAccessTimeComparer()));
-        services.AddHostedService<FileCleanupWorker>();
-        services.AddHostedService<FileStatisticsCollectionWorker>();
-    })
-    .Build()
-    .RunAsync();
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateLogger();
+
+try
+{
+    await Host.CreateDefaultBuilder(args)
+        .ConfigureHostConfiguration(builder => builder.AddEnvironmentVariables("LUSTRE_"))
+        .ConfigureServices((hostContext, services) =>
+        {
+            services.AddOptions<FileCleanupConfiguration>()
+                .Bind(hostContext.Configuration)
+                .ValidateDataAnnotations();
+
+            services.AddSingleton<IFilesystemChangeWatcher, NativeFilesystemChangeWatcher>();
+            services.AddSingleton(x => new SortedSet<FileRecord>(new LustreFileAccessTimeComparer()));
+            services.AddHostedService<FileCleanupWorker>();
+            services.AddHostedService<FileStatisticsCollectionWorker>();
+        })
+        .UseSerilog((hostContext, loggerConfiguration)
+            => loggerConfiguration
+                .ReadFrom.Configuration(hostContext.Configuration)
+                .Enrich.FromLogContext()
+        )
+        .Build()
+        .RunAsync();
+}
+catch (Exception ex)
+{
+    Log.Logger.Fatal(ex, "Application start-up failed");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
